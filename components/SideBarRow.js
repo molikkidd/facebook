493f01c9d0273f555878392434ee0c6bb4820b93
Molikkidd <mzkidd5@gmail.com>
2022-06-24 09:41:15 -0700

    sidebar row add names

--- components/SideBarRow.js
+++ components/SideBarRow.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 
 function SideBarRow({src, Icon, title}) {
   return (
-    <div className='flex items-center space-x-2 p-4'>
+    <div className='flex items-center space-x-2 p-4
+    hover:bg-gray-200 rounded-xl cursor-pointer'>
         {src && (
         <Image
         className="cursor-pointer rounded-full"
