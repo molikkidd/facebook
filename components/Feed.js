ce3884ec1b45381b4dd78b8aad2cdb9583053ea6
Molikkidd <mzkidd5@gmail.com>
2022-06-27 10:46:36 -0700

    add input box and icons

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import InputBox from './InputBox'
 import Stories from './Stories'
 
 function Feed() {
@@ -9,6 +10,7 @@ function Feed() {
         {/* STORIES */}
         <Stories/>
         {/* INPUT BOX */}
+        <InputBox/>
         {/* POST */}
         </div>      
     </div>
