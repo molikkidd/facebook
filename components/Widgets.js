b81b04c5bad90aa0ea87ed47d2ec3a127bd4245c
Molikkidd <mzkidd5@gmail.com>
2022-07-01 20:20:28 -0700

    add active widgets to right side

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { SearchIcon } from "@heroicons/react/outline"
+import { DotsHorizontalIcon, VideoCameraIcon} from "@heroicons/react/solid"
+import Contact from './Contact';
+
+const contacts = [
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bozo"},
+];
 
 function Widgets() {
   return (
-    <div>Widgets</div>
+    <div className='hidden lg:flex flex-col w-60 p-2 mt-5'>
+        <div className='flex justify-between items-center text-gray-500 mb-5'>
+           <h2 className='text-xl'> Contacts</h2> 
+           <div className='flex space-x-2'>
+            <VideoCameraIcon className='h-6'/>
+            <SearchIcon className='h-6'/>
+            <DotsHorizontalIcon className='h-6'/>
+           </div>
+        </div>
+        {contacts.map((contact) => (
+            <Contact
+            key={contact.src}
+            src={contact.src}
+            name={contact.name}
+            />
+        ))}
+    </div>
   )
 }
 
